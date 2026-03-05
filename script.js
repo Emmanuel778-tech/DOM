353d5fc74e5ed9b19c1f02c7ab8da8169ebc11cb
Emmanuel778-tech <eemmanuelcastro12@gmail.com>
2026-03-05 15:40:39 +0000

    progress

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ console.log("Script started");
 //Variable to track the nukmber of clicks
 let clicks = 0;
 
+// Value of each click
+let clickValue = 1;
+
 //Variable to store the click counter h1
 let clickdisplay = document.getElementById("clickdisplay");
+// Variable to store the levvel display
+let levelDisplay = document.getElementById("level-display");
 
 //set initial text for click counter
 clickdisplay.innerText = "Clicks: " + clicks;
@@ -13,13 +18,16 @@ clickdisplay.innerText = "Clicks: " + clicks;
 function handleClick() {
     console.log("click");
 
-    //AAdd one to click count
-    clicks = clicks + 1;
+    //Add one to click count
+    clicks = clicks + clickValue;
 
     //Update Display
     clickdisplay.innerText = "Clicks: " + clicks;
 
     if(clicks == 10) {
         alert("Level 1 Complete! ");
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
+
     }
 }
